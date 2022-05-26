ec4a62202c2a4f6d39954242323f8332dd5a640f
Alduino <git@alduino.dev>
2022-05-26 11:55:41 +1000

    Create reproduction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDeferredValue, useMemo} from "react";
+
+/*
+ * 1. Open React dev tools. I'm using version 4.24.0-82762bea5 as it's the latest on the firefox addon store.
+ * 2. Click on the "App" component.
+ * 3. See how the first memo has the value "3.14" when it should be "1.41".
+ */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useDeferredValue("");
+
+    useMemo(() => {
+        return 1.41;
+    }, []);
+
+    useMemo(() => {
+        return 3.14;
+    }, []);
+
+    return (
+        <div>
+            <p>Hello, world!</p>
+        </div>
+    );
 }
 
 export default App;
